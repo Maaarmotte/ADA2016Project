057fb098a2fd4caa7a5651e53b1d808664efae9a
Louis Séguy <louis.seguy@epfl.ch>
2017-01-29 14:22:25 +0100

    Refactor the Spark extractor to use all the data sources

--- twitter_extract_non-en_msg_spark.py
+++ twitter_extract_non-en_msg_spark.py
@@ -5,17 +5,15 @@ from pyspark.sql.functions import lit
 from pyspark.sql.functions import col
 from py4j.protocol import Py4JJavaError
 
-language = 'en'  # Don't extract this language
-extract_path = ['_source.source_location', '_source.main', '_source.sentiment', '_source.lang']
-extract_cols = ['source_location', 'lang', 'main', 'sentiment']
-#base_path = '/home/marmotte/data/ADA2016Project/data/{}/harvest3r_twitter_data_{}-{}_0.json'
-#output_path = '/home/marmotte/data/ADA2016Project/processed'
-#output_file = 'tweets_non-en_msg_{}.csv'
-#months = [('april', '04')]
+ignore_language = 'en'  # Don't extract this language
+sources_attributes = {'twitter': ['source_location', 'lang', 'main', 'sentiment'],
+                      'instagram': ['lang', 'main', 'sentiment', 'tags'], 
+                      'news': ['lang', 'title', 'extract', 'sentiment', 'tags']}
 
-base_path = 'hdfs:///datasets/goodcitylife/{}/harvest3r_twitter_data_{}-{}_0.json'
+base_path = 'hdfs:///datasets/goodcitylife/{month}/harvest3r_{source}_data_{day_num}-{month_num}_{part}.json'
 output_path = '/home/lhabegge/processed'
-output_file = 'tweets_non-en_msg_{}.csv'
+output_file = '{source}_non-en_msg_{month}.csv'
+
 months = [('january', '01'),
           ('february', '02'),
           ('march', '03'),
@@ -35,34 +33,30 @@ def path_exists(path):
     except Py4JJavaError as e:
         return False
 
-def parse_month(month_txt, month_nb):
+def parse_month(source, month_txt, month_num):
     df_month = None
     
     for i in range(31):
-        day = str(i+1).zfill(2)       # Pad with zero
-        file_path = base_path.format(month_txt, day, month_nb)
-        
-        #if os.path.isfile(file_path):
-        if path_exists(file_path):
-            print('Parsing {}...'.format(file_path))
-            
-            data = sqlContext.read.json(file_path).select(extract_path)
-            data = data.filter(data.lang != language).select(extract_cols)
-            
-            # To filter by regexp
-            #data = sqlContext.read.json(file_path).select(extract_path)
-            #data = data.filter(data.lang != language)
-            #data = data.filter(col("main").rlike(regexp)).select(extract_cols)
-                        
-            if not df_month:
-                df_month = data
-            else:
-                df_month = df_month.unionAll(data)
+        for part in range(2):
+            day = str(i+1).zfill(2)       # Pad with zero
+            file_path = base_path.format({'source': source, 'month': month_txt, 'day_num': day, 'month_num': month_num, 'part': part})
+
+            if path_exists(file_path):
+                print('Parsing {}...'.format(file_path))
+                
+                attributes = sources_attributes[source]
+                data = sqlContext.read.json(file_path).select(['_source.' + attr for attr in attributes])
+                data = data.filter(data.lang != ignore_language).select(attributes)
+                            
+                if not df_month:
+                    df_month = data
+                else:
+                    df_month = df_month.unionAll(data)
     
     # Regroup the data locally and save to txt file
     if df_month:
         final = df_month.map(toCSV).collect()
-        output = '{}/{}'.format(output_path, output_file.format(month_txt))
+        output = os.path.join(output_path, output_file.format({'source': source, 'month': month_txt}))
         save(final, output)
         
 def toCSV(row):
@@ -82,6 +76,7 @@ except:
 sqlContext = SQLContext(sc)
 
 # Extract everything !
-for tup in months:
-    parse_month(tup[0], tup[1])
+for month, month_num in months:
+    for source in sources_attributes.keys():
+        parse_month(source, month, month_num)
     
